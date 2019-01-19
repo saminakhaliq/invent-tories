60b2462ec18920021ad0d703d4ac913dc5219779
Samina <saminak77@gmail.com>
2019-01-19 18:29:06 -0500

    Update Item.java

--- InventTories/main/java/com/example/bilhaghedeon/finalprojectghedeonkhaliq/model/Item.java
+++ InventTories/main/java/com/example/bilhaghedeon/finalprojectghedeonkhaliq/model/Item.java
@@ -4,9 +4,6 @@ import android.arch.persistence.room.ColumnInfo;
 import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.PrimaryKey;
 
-/**
- * Implemented by Bilha Ghedeon
- */
 @Entity (tableName = "items_table")
 public class Item {
 
